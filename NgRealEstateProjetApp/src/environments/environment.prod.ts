4b0aef08be1aa3c2ef9ab03bc19fbc33ac30b3c1
Achraf AGUERSI <achraf.a@hiitconsulting.com>
2021-04-21 15:18:04 +0000

    fix(route): host api url

--- NgRealEstateProjetApp/src/environments/environment.prod.ts
+++ NgRealEstateProjetApp/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   SERVER_URL: 'https://api.realestate.hiitconsulting.com',
+  hostURL : 'https://api.realestate.hiitconsulting.com/',
+
   production: true,
   useHash: true,
   hmr: false,
