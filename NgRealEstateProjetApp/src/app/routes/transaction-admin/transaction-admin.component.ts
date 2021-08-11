9f0c9cb0ca1aef14852d99975f8edecc147cfbd6
Achraf AGUERSI <achraf.a@hiitconsulting.com>
2021-08-11 14:06:48 +0100

    feat(transaction):listTypologie

--- NgRealEstateProjetApp/src/app/routes/transaction-admin/transaction-admin.component.ts
+++ NgRealEstateProjetApp/src/app/routes/transaction-admin/transaction-admin.component.ts
@@ -7,84 +7,113 @@ import { TransactionService } from '@shared/services/transaction.service';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-transaction-admin',
-  templateUrl: './transaction-admin.component.html',
-  styleUrls: ['./transaction-admin.component.scss']
+	selector: 'app-transaction-admin',
+	templateUrl: './transaction-admin.component.html',
+	styleUrls: ['./transaction-admin.component.scss'],
 })
 export class TransactionAdminComponent implements OnInit {
- 
-  unsubscribe$: Subject<void>;
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private snackBar: MatSnackBar,
-    private transactionService: TransactionService,
-  ) {
-    this.unsubscribe$ = new Subject();
-  }
-  
-  
-  @ViewChild(MatPaginator, { static: true }) pager: MatPaginator;
-  transactions: any[];
+	unsubscribe$: Subject<void>;
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private snackBar: MatSnackBar,
+		private transactionService: TransactionService
+	) {
+		this.unsubscribe$ = new Subject();
+	}
 
-  displayedColumns = [
-    "Date De Création",
-    "premier agent",
-    "deuxième agent",
-    "Price",
-    "Typologie",
-    "CompromiseDate",
-    "ProvisionalActDate",
-    "OfficialActDate",
-    "Notary",
-    "Type",
-    "Status",
-    "Menu",
-  ];
+	@ViewChild(MatPaginator, { static: true }) pager: MatPaginator;
+	transactions: any[];
 
-  ngOnInit(): void {
-    this.transactionService.getAllTransaction().subscribe((res) => {
-      this.transactions = res['hydra:member'];
-    });
-  }
+	displayedColumns = [
+		'Date De Création',
+		'premier agent',
+		'deuxième agent',
+		'Price',
+		'Typologie',
+		'CompromiseDate',
+		'ProvisionalActDate',
+		'OfficialActDate',
+		'Notary',
+		'Type',
+		'Status',
+		'Menu',
+	];
 
-  onClick(){}
+	ngOnInit(): void {
+		this.transactionService.getAllTransaction().subscribe((res) => {
+			this.transactions = res['hydra:member'];
+		});
+	}
 
-  refus(id: number) {
-    this.transactionService.updatTransaction({'status':'refused'},id).pipe(takeUntil(this.unsubscribe$))
-        .subscribe((res) => {
-          this.transactionService.getAllTransaction().pipe(takeUntil(this.unsubscribe$))
-            .subscribe((res) => {
-             this.snackBar.open('transaction est refusé!', '', { duration: 1000 ,panelClass: ['blue-snackbar'] ,  verticalPosition: 'top', horizontalPosition:'end' });
-             this.transactions = res['hydra:member'];
-              },
-            (error) => {
-              this.snackBar.open("veuillez vérifier vos connexions!", '', { duration: 1000, panelClass: ['red-snackbar'], verticalPosition: 'top', horizontalPosition:'end'});
-            });  
-    });
-  }
-  
-  accept(id: number) {
-    this.transactionService.updatTransaction({'status':'accepted'},id).pipe(takeUntil(this.unsubscribe$))
-    .subscribe((res) => {
-      this.transactionService.getAllTransaction().pipe(takeUntil(this.unsubscribe$))
-        .subscribe((res) => {
-          this.snackBar.open('transaction est accepté!', '', { duration: 1000 ,panelClass: ['blue-snackbar'] ,  verticalPosition: 'top', horizontalPosition:'end' });
-          this.transactions = res['hydra:member'];
-      },(error) =>{
-        this.snackBar.open("veuillez vérifier vos connexions!", '', { duration: 1000, panelClass: ['red-snackbar'], verticalPosition: 'top', horizontalPosition:'end'});
-      });
-      
-    });
-  }
+	onClick() {}
 
-  details(id: number) {
-    this.router.navigate(["./", id, "details"], { relativeTo: this.route });
-  }
-  
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-  
+	refus(id: number) {
+		this.transactionService
+			.updatTransaction({ status: 'refused' }, id)
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe((res) => {
+				this.transactionService
+					.getAllTransaction()
+					.pipe(takeUntil(this.unsubscribe$))
+					.subscribe(
+						(res) => {
+							this.snackBar.open('transaction est refusé!', '', {
+								duration: 1000,
+								panelClass: ['blue-snackbar'],
+								verticalPosition: 'top',
+								horizontalPosition: 'end',
+							});
+							this.transactions = res['hydra:member'];
+						},
+						(error) => {
+							this.snackBar.open('veuillez vérifier vos connexions!', '', {
+								duration: 1000,
+								panelClass: ['red-snackbar'],
+								verticalPosition: 'top',
+								horizontalPosition: 'end',
+							});
+						}
+					);
+			});
+	}
+
+	accept(id: number) {
+		this.transactionService
+			.updatTransaction({ status: 'accepted' }, id)
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe((res) => {
+				this.transactionService
+					.getAllTransaction()
+					.pipe(takeUntil(this.unsubscribe$))
+					.subscribe(
+						(res) => {
+							this.snackBar.open('transaction est accepté!', '', {
+								duration: 1000,
+								panelClass: ['blue-snackbar'],
+								verticalPosition: 'top',
+								horizontalPosition: 'end',
+							});
+							this.transactions = res['hydra:member'];
+						},
+						(error) => {
+							this.snackBar.open('veuillez vérifier vos connexions!', '', {
+								duration: 1000,
+								panelClass: ['red-snackbar'],
+								verticalPosition: 'top',
+								horizontalPosition: 'end',
+							});
+						}
+					);
+			});
+	}
+
+	details(id: number) {
+		this.router.navigate(['./', id, 'details'], { relativeTo: this.route });
+	}
+
+	ngOnDestroy(): void {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
 }
--- NgRealEstateProjetApp/src/app/routes/transaction/transaction-form/transaction-form.component.ts
+++ NgRealEstateProjetApp/src/app/routes/transaction/transaction-form/transaction-form.component.ts
@@ -14,141 +14,170 @@ import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-transaction-form',
-  templateUrl: './transaction-form.component.html',
-  styleUrls: ['./transaction-form.component.scss']
+	selector: 'app-transaction-form',
+	templateUrl: './transaction-form.component.html',
+	styleUrls: ['./transaction-form.component.scss'],
 })
 export class TransactionFormComponent implements OnInit {
+	projectAchat: Project;
+	projectVente: Project;
+	projectsAchat: Project[];
+	projectsVente: Project[];
+	tablProject = [];
+	notarys: any;
+	notary: any;
+	showSpinner = true;
+	typeTransaction: string[];
+	listTypologie: string[];
+	unsubscribe$: Subject<void>;
+	form: FormGroup;
+	constructor(
+		private fb: FormBuilder,
+		private route: ActivatedRoute,
+		private router: Router,
+		private snackBar: MatSnackBar,
+		private transactionService: TransactionService,
+		private notaryService: NotaryService,
+		private dialog: MatDialog,
+		private translate: TranslateService,
+		private projectService: ProjectService,
+		private loginService: LoginService
+	) {
+		this.typeTransaction = ['achat', 'vente'];
+		this.listTypologie = [
+			'Demeure',
+			'Domaine',
+			'Ferme',
+			'Maison de campagne',
+			'Maison de village',
+			'Maison de ville',
+			'Maison traditionnelle',
+		];
+		this.unsubscribe$ = new Subject();
+		this.form = this.fb.group({
+			type: [null], //"achat||vente",
+			typologie: [null],
+			price: [null],
+			feesPaidBy: [null],
+			quote: [null],
+			companyBaseFees: [null],
+			companyFullFees: [null],
+			compromiseDate: [null],
+			provisionalActDate: [null],
+			officialActDate: [null],
+			projects: [null], //["/api/v2/projects/3","/api/v2/projects/4"],
+			notary: [null], //"/api/v2/notaries/53"
+		});
+	}
 
-  projectAchat: Project;
-  projectVente: Project;
-  projectsAchat: Project[];
-  projectsVente: Project[];
-  tablProject = [];
-  notarys: any;
-  notary: any;
-  showSpinner=true;
-  typeTransaction:string[];
-  listTypologie: string[];
-  unsubscribe$: Subject<void>;
-  form :FormGroup;
-  constructor(
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private snackBar: MatSnackBar,
-    private transactionService: TransactionService,
-    private notaryService: NotaryService,
-    private dialog: MatDialog,
-    private translate: TranslateService,
-    private projectService: ProjectService,
-    private loginService: LoginService
-  ) {
-    this.typeTransaction=['achat','vente'];
-    this.listTypologie= ['....'];
-    this.unsubscribe$ = new Subject();
-    this.form = this.fb.group({
-    type : [null ],//"achat||vente",
-    typologie: [null ],
-    price : [null ],
-    feesPaidBy : [null ],
-    quote : [null ],
-    companyBaseFees:[null ],
-    companyFullFees:[null ],
-    compromiseDate: [null ],
-    provisionalActDate: [null ],
-    officialActDate: [null ],
-    projects: [null ],//["/api/v2/projects/3","/api/v2/projects/4"],
-    notary: [null ],//"/api/v2/notaries/53"
-  });
-}
+	ngOnInit(): void {
+		let idUser = this.loginService.getCurentUser().id;
+		this.transactionService
+			.getProjects('vente')
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe((res) => {
+				this.projectsVente = res['hydra:member'];
+			});
+		this.transactionService
+			.getProjects(`achat&agent.id=${idUser}`)
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe((res) => {
+				this.projectsAchat = res['hydra:member'];
+			});
+		this.notaryService
+			.getAllNotary()
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe((res) => {
+				this.notarys = res['hydra:member'];
+			});
 
-  ngOnInit(): void { 
-
-    let idUser= this.loginService.getCurentUser().id;
-   this.transactionService.getProjects('vente').pipe(takeUntil(this.unsubscribe$)).subscribe((res) => {
-    this.projectsVente = res['hydra:member'];
-   });
-   this.transactionService.getProjects(`achat&agent.id=${idUser}`).pipe(takeUntil(this.unsubscribe$)).subscribe((res) => {
-     this.projectsAchat = res['hydra:member'];
-   });
-   this.notaryService.getAllNotary().pipe(takeUntil(this.unsubscribe$)).subscribe((res) => {
-    this.notarys = res['hydra:member'];
-   });
-   
-   /*this.projectService.getProject(this.id).pipe(takeUntil(this.unsubscribe$)).subscribe((project) => {
+		/*this.projectService.getProject(this.id).pipe(takeUntil(this.unsubscribe$)).subscribe((project) => {
      this.project = project;
      if(project!={})
      {
        this.showSpinner=false;
      }
     });*/
-    
-      
-  }
-  openDialog() {
-    this.dialog
-      .open(DialogContentProjectComponent, {
-        width: "600px",
-        disableClose: true,
-        data: {
-          title:this.translate.instant('Transaction.title-dialog'),
-          type:'notary'
-        }
-      })
-      .afterClosed()
-      .subscribe(() => {
-        this.notaryService.getAllNotary().pipe(takeUntil(this.unsubscribe$)).subscribe((res) => {
-          this.notarys = res['hydra:member'];
-        });
-      });
-}
-  OnClick(){}
-  change(event,type:number)
-  {
-    if(event.isUserInput) {
-      console.log(event.source.value, event.source.selected);
-      if(type ==1){
-        this.tablProject[0]=`/api/v2/projects/${event.source.value}`;
-        this.projectService.getProject(event.source.value).subscribe((project) => {
-          this.projectAchat = project;
-          if(project!={})
-            this.showSpinner=false;
-        })
-      }else if(type==2){
-        this.tablProject[1]=`/api/v2/projects/${event.source.value}`;
-        this.projectService.getProject(event.source.value).subscribe((project) => {
-          this.projectVente = project;
-          if(project!={})
-            this.showSpinner=false;
-        })
-      }
-    }
-  }
-  selectNotary(event)
-  {//event.source.value
-    if(event.isUserInput) {
-        this.notary = event.source.value;
-        this.form.get('notary').setValue(event.source.value['@id']);
-      }
-  }
-  details(id:number) {
-    this.router.navigate(['/project',id,'details']);   
-  }
-  onSubmit() {
-   this.form.get('projects').setValue(this.tablProject);
-   
-    this.transactionService.save(this.form.value).pipe(takeUntil(this.unsubscribe$)).subscribe(response=>{
-      this.snackBar.open('transaction est ajouté avec succès!', '', { duration: 1000 ,panelClass: ['blue-snackbar'] ,  verticalPosition: 'top', horizontalPosition:'end' });
-      setTimeout(()=>{  
-        this.router.navigate(['/transaction/transactionlist'])
-      }, 2000); },
-    (error) => {
-      this.snackBar.open("veuillez vérifier vos informations!", '', { duration: 1000, panelClass: ['red-snackbar'], verticalPosition: 'top', horizontalPosition:'end'});
-    });
-  }
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+	}
+	openDialog() {
+		this.dialog
+			.open(DialogContentProjectComponent, {
+				width: '600px',
+				disableClose: true,
+				data: {
+					title: this.translate.instant('Transaction.title-dialog'),
+					type: 'notary',
+				},
+			})
+			.afterClosed()
+			.subscribe(() => {
+				this.notaryService
+					.getAllNotary()
+					.pipe(takeUntil(this.unsubscribe$))
+					.subscribe((res) => {
+						this.notarys = res['hydra:member'];
+					});
+			});
+	}
+	OnClick() {}
+	change(event, type: number) {
+		if (event.isUserInput) {
+			console.log(event.source.value, event.source.selected);
+			if (type == 1) {
+				this.tablProject[0] = `/api/v2/projects/${event.source.value}`;
+				this.projectService.getProject(event.source.value).subscribe((project) => {
+					this.projectAchat = project;
+					if (project != {}) this.showSpinner = false;
+				});
+			} else if (type == 2) {
+				this.tablProject[1] = `/api/v2/projects/${event.source.value}`;
+				this.projectService.getProject(event.source.value).subscribe((project) => {
+					this.projectVente = project;
+					if (project != {}) this.showSpinner = false;
+				});
+			}
+		}
+	}
+	selectNotary(event) {
+		//event.source.value
+		if (event.isUserInput) {
+			this.notary = event.source.value;
+			this.form.get('notary').setValue(event.source.value['@id']);
+		}
+	}
+	details(id: number) {
+		this.router.navigate(['/project', id, 'details']);
+	}
+	onSubmit() {
+		this.form.get('projects').setValue(this.tablProject);
+
+		this.transactionService
+			.save(this.form.value)
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe(
+				(response) => {
+					this.snackBar.open('transaction est ajouté avec succès!', '', {
+						duration: 1000,
+						panelClass: ['blue-snackbar'],
+						verticalPosition: 'top',
+						horizontalPosition: 'end',
+					});
+					setTimeout(() => {
+						this.router.navigate(['/transaction/transactionlist']);
+					}, 2000);
+				},
+				(error) => {
+					this.snackBar.open('veuillez vérifier vos informations!', '', {
+						duration: 1000,
+						panelClass: ['red-snackbar'],
+						verticalPosition: 'top',
+						horizontalPosition: 'end',
+					});
+				}
+			);
+	}
+	ngOnDestroy(): void {
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
 }
