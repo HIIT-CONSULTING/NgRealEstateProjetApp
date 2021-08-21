f3bef3ebf89abaa1d3703b5a959125647fdfa112
Achraf AGUERSI <achraf.a@hiitconsulting.com>
2021-08-21 19:18:53 +0100

    feat(mandat/mandat-admin): change ID to UID

--- NgRealEstateProjetApp/src/app/routes/mandat-admin/mandat-details/mandat-details.component.ts
+++ NgRealEstateProjetApp/src/app/routes/mandat-admin/mandat-details/mandat-details.component.ts
@@ -9,44 +9,42 @@ import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-mandat-details',
   templateUrl: './mandat-details.component.html',
-  styleUrls: ['./mandat-details.component.scss']
+  styleUrls: ['./mandat-details.component.scss'],
 })
 export class MandatDetailsComponent implements OnInit {
-
-  
-  mandat:Mandat;
+  mandat: Mandat;
   project: Project;
   project$: Observable<Project>;
-  showSpinner=true;
-  id:number;
+  showSpinner = true;
+  id: string;
   unsubscribe$: Subject<void>;
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private mandatService: MandatService,
-    private projectService: ProjectService,
+    private projectService: ProjectService
   ) {
-
     this.unsubscribe$ = new Subject();
-    
-}
+  }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.route.params.pipe(takeUntil(this.unsubscribe$)).subscribe((params) => {
-      this.id = +params["id"];
+      this.id = params['id'];
     });
-   this.mandatService.getMandat(this.id).pipe(takeUntil(this.unsubscribe$)).subscribe(res =>{ 
-     this.mandat = res });
-   //this.project$ = this.projectService.getProject(this.id);
-   this.projectService.getProject(this.id).pipe(takeUntil(this.unsubscribe$)).subscribe((project) => {
-     this.project = project;
-     if(project!={})
-     {
-       this.showSpinner=false;
-     }
-    });
-    
-      
+    this.mandatService
+      .getMandat(this.id)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((res) => {
+        this.mandat = res;
+        this.projectService
+          .getProject(this.mandat?.project.id)
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe((project) => {
+            this.project = project;
+            if (project != {}) {
+              this.showSpinner = false;
+            }
+          });
+      });
   }
   ngOnDestroy(): void {
     this.unsubscribe$.next();
